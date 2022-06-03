7b3e76aaec148c001c383a5c635492f6728cdd85
HobbeyZocker <tonnbonn.ar@gmail.com>
2022-06-03 10:17:53 +0200

    made file for tanker

---
+++ src/main/java/tankerConsumer.java
@@ -0,0 +1,60 @@
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class tankerConsumer extends Thread {
+    private final static String TOPIC = "tankerkoenig";
+    private final static String SERVER = "10.50.15.52:9092";
+    private final static String CLIENT_ID = "Group 8";
+    private static Consumer<Long, String> consumer;
+
+    public tankerConsumer() {
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,SERVER);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,CLIENT_ID);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer");
+
+        consumer = new KafkaConsumer<>(props);
+
+        consumer.subscribe(Collections.singleton(TOPIC));
+    }
+
+    public static void runConsumer() throws InterruptedException {
+
+        final int giveUp = 100;   int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            if (consumerRecords.count()==0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp) break;
+                else continue;
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n",
+                        record.key(), record.value(),
+                        record.partition(), record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
+
+    public Consumer<Long, String> getConsumer() {
+        return consumer;
+    }
+}
