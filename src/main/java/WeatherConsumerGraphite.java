b7839a0fb18947a22fdd2f77a4ee1036299762c5
Loehj <johannes.loehj@gmail.com>
2022-06-03 12:57:43 +0200

    reformat

--- src/main/java/WeatherConsumerGraphite.java
+++ src/main/java/WeatherConsumerGraphite.java
@@ -1,20 +1,14 @@
-import org.apache.kafka.clients.consumer.*;
+import com.codahale.metrics.graphite.Graphite;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
-
-import java.io.IOException;
-
-import com.codahale.metrics.graphite.Graphite;
-
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 public class WeatherConsumerGraphite {
     private final static String TOPIC = "weather";
@@ -25,54 +19,54 @@ public class WeatherConsumerGraphite {
     public WeatherConsumerGraphite() {
 
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,SERVER);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG,CLIENT_ID);
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"true");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG,"1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"30000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.IntegerDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
         consumer = new KafkaConsumer<>(props);
 
     }
 
-    public void sendWeather() throws Exception {
+    public void sendWeather() throws IOException {
 
         Graphite graphite = new Graphite("10.50.15.52", 2003);
-
+        graphite.flush();
         graphite.connect();
         if (graphite.isConnected()) {
             graphite.flush();
             try {
                 consumer.subscribe(Collections.singletonList(TOPIC));
                 while (true) {
-                    ConsumerRecords<Long, String> records = consumer.poll(Duration.of(60, ChronoUnit.SECONDS));
-                    if (records.isEmpty()) {
-                        Thread.sleep(10_000);
-                    }
+                    ConsumerRecords<Long, String> records = consumer.poll(Duration.of(1, ChronoUnit.MINUTES));
+
                     records.forEach(record -> {
                         try {
-                            System.out.println("inf19b.group8.weather." + getCity(record.value()).toLowerCase() + ","+ getTemp(record.value()) + ""+ "," + record.timestamp()/1000);
-                            graphite.send("inf19b.group8.weather." + getCity(record.value()).toLowerCase(), getTemp(record.value()) + "", record.timestamp()/1000);
+                            System.out.println("inf19b.group8.weather." + getCity(record.value()).toLowerCase() + "," + getTemp(record.value()) + "" + "," + record.timestamp() / 1000);
+                            graphite.send("inf19b.group8.weather." + getCity(record.value()).toLowerCase(), getTemp(record.value()), record.timestamp() / 1000);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     });
                 }
-            } catch (Exception e){
-                throw new Exception(e);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+        consumer.close();
+        graphite.flush();
         graphite.close();
     }
 
-    public String getCity(String value){
+    public String getCity(String value) {
         return value.split(",")[5].split("\"")[3];
     }
 
-    public String getTemp(String value){
-        return value.split("\"")[2].substring(1, value.split("\"")[2].length()-2);
+    public String getTemp(String value) {
+        return value.split("\"")[2].substring(1, value.split("\"")[2].length() - 2);
     }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,19 +2,17 @@ import java.io.IOException;
 
 public class main {
 
-    public static void main(String[] args) throws Exception {
-        /*WeatherConsumer weatherConsumer = new WeatherConsumer();
+    public static void main(String[] args) {
+        WeatherConsumer weatherConsumer = new WeatherConsumer();
         KProducer producer = new KProducer();
+        WeatherConsumerGraphite weatherConsumerGraphite = new WeatherConsumerGraphite();
         try {
-            producer.runProducer(5);
-            weatherConsumer.runConsumer();
+            //producer.runProducer(5);
+            //weatherConsumer.runConsumer();
+            weatherConsumerGraphite.sendWeather();
         } catch (Exception e) {
             e.printStackTrace();
-        }*/
-
-        WeatherConsumerGraphite weatherConsumerGraphite = new WeatherConsumerGraphite();
-
-        weatherConsumerGraphite.sendWeather();
+        }
     }
 
 }
